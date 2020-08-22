41142e5bd27de907d57905be4084724ea667ed50
Juan David Gomez <juandastic@gmail.com>
2020-08-22 12:04:46 -0500

    Updates

---
+++ precache-manifest.1d3db2b1f2e3caf56dd7bad485c4a279.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f10553cc0d8c6932c40657b1635e66b",
+    "url": "/graduation-project-managment/index.html"
+  },
+  {
+    "revision": "f35eb4169af159470a0b",
+    "url": "/graduation-project-managment/static/css/main.fa56d39a.chunk.css"
+  },
+  {
+    "revision": "b6eeb5110353ca964b8c",
+    "url": "/graduation-project-managment/static/js/2.de6aa3a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/graduation-project-managment/static/js/2.de6aa3a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f35eb4169af159470a0b",
+    "url": "/graduation-project-managment/static/js/main.bce02543.chunk.js"
+  },
+  {
+    "revision": "091df8346ad7ae597629",
+    "url": "/graduation-project-managment/static/js/runtime-main.f3cae7f4.js"
+  }
+]);
