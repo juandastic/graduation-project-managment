506e23f08363138f3eec95c6d9ff8744145a33d7
Juan David Gomez <juandastic@gmail.com>
2020-08-23 18:35:13 -0500

    Fix typo

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -35,7 +35,7 @@ function TaskForm({handleTaskCreation}) {
         </div>
         <div className="w-full md:w-1/2 px-3">
           <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="description">
-            Descripción del Proyecto
+            Descripción de la tarea
           </label>
           <textarea onChange={onChangeHandler} name="description" value={taskInfo.description} rows="6" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
           </textarea>
