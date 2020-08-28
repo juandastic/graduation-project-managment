f202f4cfbf2fb55572806a1d68b3a2b83e4d7dd7
Juan David Gomez <juandastic@gmail.com>
2020-08-27 21:21:41 -0500

    add component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 function Header() {
   return (
-    <header className="z-40 shadow-xs p-6 bg-white shadow-bottom">
-      Gestión de Proyectos de Grado
+    <header className="z-40 shadow-xs p-6 px-10 bg-white shadow-bottom flex justify-between">
+      <span>Gestión de Proyectos de Grado</span>
+      <span>Asesor de Proyecto</span>
     </header>
   )
 }
